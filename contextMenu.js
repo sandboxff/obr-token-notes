454aa0d7a1a885495416a89dd4110c96eca8f1fa
sandboxff <153873660+sandboxff@users.noreply.github.com>
2023-12-20 08:08:42 +0100

    Implement loading and saving of notes

--- contextMenu.js
+++ contextMenu.js
@@ -1,6 +1,8 @@
 import OBR from "@owlbear-rodeo/sdk"
 
 const ID = "localhost";
+const editorElement = document.getElementById("editor")
+const currentNote = document.getElementById("current-note")
 
 export function setupContextMenu() {
   OBR.contextMenu.create({
@@ -17,10 +19,42 @@ export function setupContextMenu() {
     onClick(context) {
       console.log(1);
       if (context.items.length == 1) {
-        const editorElement = document.getElementById("editor")
-        document.getElementById("current-note").value = context.items[0].id
-        editorElement.editor.insertString(context.items[0].id)
+        getNote(context.items[0].id)
+        currentNote.value = context.items[0].id
       }
     },
   });
 }
+
+const getNote = (id) => {
+  fetch('http://localhost:3001/' + id, {
+    method: 'GET'
+  })
+  .then((res) => res.text())
+  .then((res) => {
+    editorElement.value = res
+  })
+  .catch((err) => {
+    console.error(err)
+  })
+}
+
+const saveNote = () => {
+  if (currentNote.value != "") {
+    fetch('http://localhost:3001/', {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    method: 'POST',
+    body: JSON.stringify({id: currentNote.value, content: editorElement.value})
+    })
+    .then((res) => res.text())
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err))
+  }
+} 
+
+addEventListener("trix-blur", (e) => {
+  saveNote();
+})
