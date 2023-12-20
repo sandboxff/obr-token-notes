8957d7d15351d957af3d29abf39e66d0c802fe08
sandboxff <153873660+sandboxff@users.noreply.github.com>
2023-12-20 05:55:25 +0100

    Add API endpoints

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,40 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
-app.get('/', (request, response) => {
-    response.send('')
+const fs = require('fs')
+const dir = './localData/'
+
+fs.access(dir, (err) => {
+    if(err) {
+        fs.mkdirSync(dir)
+    } else {
+
+    }
+})
+
+app.get('/:id', (req, res) => {
+    fs.readFile(dir + req.params.id, (err, data) => {
+        if (err) {
+            res.status(404)
+            res.send('')
+        } else {
+            res.send(data)
+        }
+    })
+})
+
+app.post('/', (req, res) => {
+
+    console.log(req.body)
+    fs.writeFile(dir + req.body.id, req.body.content, 'utf8', (err) => {
+        if (err) {
+            res.status(400)
+        } else {
+            res.status(200)
+            res.send('Data successfuly saved')
+        }
+    })
 })
 
 const PORT = 3001
